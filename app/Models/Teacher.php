ca0b2874099e3e0fb46e6ae7da64c24a072674c8
andrade <gabrieljoseandraderoso5@gmail.com>
2024-08-07 14:23:03 -0300

    fix typo

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Student extends Authenticatable
+class Teacher extends Authenticatable
 {
     use HasFactory, Notifiable;
 
