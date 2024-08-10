f95d89b0aad703d65db3ec16e89bff0c3e856e4e
Martin <martintamo627@gmail.com>
2024-08-10 16:17:49 -0300

    Seeder update (user)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Student;
 use App\Models\Teacher;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,12 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $estudiantes = Student::factory()->count(10)->make();
+        $usuarios = User::factory()->count(10)->make();
+        DB::table('usuarios')->insert($usuarios->toArray());
 
+        $estudiantes = Student::factory()->count(10)->make();
         DB::table('estudiantes')->insert($estudiantes->toArray());
 
         $profesores = Teacher::factory()->count(10)->make();
-
         DB::table('profesores')->insert($profesores->toArray());
     }
 }
