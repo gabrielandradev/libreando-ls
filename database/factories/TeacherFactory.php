9e430dc87f111f6a77ec64eb3bf67a0c0b61d38e
Martin <martintamo627@gmail.com>
2024-08-10 15:17:17 -0300

    Factory creation

---
+++ database/factories/TeacherFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Teacher>
+ */
+class TeacherFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    
+    public function definition(): array
+    {
+        return [
+            'dni' => fake()->unique()-> numberBetween(40000000, 60000000),
+            'id_usuario' => fake()->unique()-> numberBetween(1, 20),
+            'apellido' => fake() -> lastName(),
+            'nombre' => fake() -> firstName(),
+        ];
+    }
+}
