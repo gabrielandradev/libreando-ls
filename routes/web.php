1887cb2e57cc53526ed2e869b617bbd7e5c060d7
GuillermoCamero <114822179+GuillermoCamero@users.noreply.github.com>
2024-08-13 14:14:34 -0300

    Laravel Breeze integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
@@ -12,6 +13,10 @@ Route::get('/registrarse/', function () {
     return view("user-signup-menu");
 });
 
+Route::get('/secret/', function () {
+    return view('secret');
+})->middleware(['auth', 'verified']);
+
 Route::controller(StudentController::class)->group(function() {
     Route::get("/registrarse/estudiantes", "create");
     Route::post("/registrarse/estudiantes", "store");
@@ -21,3 +26,11 @@ Route::controller(TeacherController::class)->group(function() {
     Route::get("/registrarse/profesores", "create");
     Route::post("/registrarse/profesores", "store");
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
