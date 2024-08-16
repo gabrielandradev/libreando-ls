8b8ce7d081f071ee7eea3872d9c7dd749a4c6e06
Martin <martintamo627@gmail.com>
2024-08-15 21:47:27 -0300

    Database update (no autor)

--- database/database.sql
+++ database/database.sql
@@ -2,15 +2,15 @@ CREATE DATABASE IF NOT EXISTS libreando;
 
 USE libreando;
 
-CREATE TABLE IF NOT EXISTS usuarios (
+CREATE TABLE IF NOT EXISTS Usuario (
 	id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     contraseña VARCHAR(72) NOT NULL,
-    rol ENUM('estudiante', 'profesor'),
+    rol ENUM('estudiante', 'profesor', 'administrador'),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS estudiantes (
+CREATE TABLE IF NOT EXISTS Estudiante (
     dni VARCHAR(8) NOT NULL,
     id_usuario INT NOT NULL,
     apellido VARCHAR(255) NOT NULL,
@@ -21,11 +21,11 @@ CREATE TABLE IF NOT EXISTS estudiantes (
     especialidad ENUM('electrica', 'mecanica', 'computacion', 'electronica', 'quimica', 'construcciones'),
     telefono VARCHAR(25) NOT NULL,
     domicilio VARCHAR(100) NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
     PRIMARY KEY(dni)
 );
 
-CREATE TABLE IF NOT EXISTS profesores (
+CREATE TABLE IF NOT EXISTS Profesor (
 	dni VARCHAR(8) NOT NULL,
     id_usuario INT NOT NULL,
     apellido VARCHAR(255) NOT NULL,
@@ -33,6 +33,45 @@ CREATE TABLE IF NOT EXISTS profesores (
     especialidad ENUM('electrica', 'mecanica', 'computacion', 'electronica', 'quimica', 'construcciones'),
     telefono VARCHAR(25) NOT NULL,
     domicilio VARCHAR(100) NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
     PRIMARY KEY(dni)
 );
+
+CREATE TABLE IF NOT EXISTS Administrador (
+	dni VARCHAR(8) NOT NULL,
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    apellido VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    celular VARCHAR(25) NOT NULL,
+    funcion VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    PRIMARY KEY (dni)
+);
+
+CREATE TABLE IF NOT EXISTS Libro (
+	num_inventario VARCHAR(255) NOT NULL,
+    ubicacion_fisica VARCHAR(255) NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL, /*Multivaluadiririjillo*/
+    año_edicion YEAR NOT NULL,
+    num_edicion INT NOT NULL,
+    lugar_edicion VARCHAR(255) NOT NULL,
+    isbn VARCHAR(255),
+    desc_primario VARCHAR(255) NOT NULL,
+    desc_secundario VARCHAR(255) NOT NULL,
+    idioma VARCHAR(255) NOT NULL,
+    notas VARCHAR(255) NOT NULL,
+    num_paginas VARCHAR(255) NOT NULL,
+    PRIMARY KEY(num_inventario)
+    );
+    
+CREATE TABLE IF NOT EXISTS Prestamo (
+	num_inventario VARCHAR(255) NOT NULL,
+    id_usuario INT NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_devolucion DATE NOT NULL,
+    estado ENUM('solicitado', 'cancelado', 'prestado', 'devuelto', 'atrasado'),
+    FOREIGN KEY (num_inventario) REFERENCES Libro(num_inventario),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    PRIMARY KEY(num_inventario, id_usuario)
+    );
