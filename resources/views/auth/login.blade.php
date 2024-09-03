cbbb52c1d09f98edb81dc19793cc243b10a75e10
Martin <Martintamo627@gmail.com>
2024-09-03 17:39:42 -0300

    Login update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,40 +2,56 @@
     
     <x-auth-session-status :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
-        <div>
-            <label for="email">Correo Electronico</label>
-            <input type="email" name="email" id="email" value="{{ old('email') }}" required autofocus
-                autocomplete="username">
-            <x-input-error :messages="$errors->get('email')" />
-        </div>
+    <div>
+        <img id="childrens" src="{{ asset('images/login/niños_pila.png') }}">
+    </div>
 
-        <div>
-            <label for="email">Contraseña</label>
+    <div class="loginbox">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-            <input type="password" name="contraseña" id="contraseña" required autocomplete="current-password">
+            <div>
+                <h1>¡Bienvenido de nuevo!</h1>
+            </div>
 
-            <x-input-error :messages="$errors->get('contraseña')" />
-        </div>
+            <div class= "user">
+                <label for="email"></label>
+                
+                <input type="email" name="email" id="email" placeholder="Correo Electronico" value="{{ old('email') }}" required autofocus
+                    autocomplete="username">
 
-        <div>
-            <label for="remember-me">
-                <input id="remember-me" type="checkbox" name="remember" />
-                <span>Recordarme la proxima vez</span>
-            </label>
-        </div>
+                <x-input-error :messages="$errors->get('email')" />
+            </div>
 
-        <div>
-            @if (Route::has('password.request'))
-                <a
-                    href="{{ route('password.request') }}">
-                    Olvidaste tu contrasena?
-                </a>
-            @endif
+            <div class="user">
+                <label for="email"></label>
 
-            <button type="submit">Iniciar sesion</button>
-        </div>
-    </form>
+                <input type="password" name="contraseña" id="contraseña" placeholder="Contraseña" required autocomplete="current-password">
+
+                <x-input-error :messages="$errors->get('contraseña')" />
+            </div>
+
+            <div>
+                <label for="remember-me">
+                    <input id="remember-me" type="checkbox" name="remember" />
+                    <span>Recordarme la proxima vez</span>
+                </label>
+            </div>
+
+            <button id="button" type="submit">Iniciar sesion</button>
+            <div class="signup">            
+                @if (Route::has('password.request'))
+                    <a id="forgot_password"
+                        href="{{ route('password.request') }}">
+                        Recordar contraseña
+                    </a>
+                @endif
+            </div>
+            <div class="signup">
+                ¿Todavia no tienes cuenta? <a href="{{ route('registrarse') }}">Regístrate</a>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
