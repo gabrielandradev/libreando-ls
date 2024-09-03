842b318c136fea73a4171571c9b9fa58f3d438bb
GuillermoCamero <114822179+GuillermoCamero@users.noreply.github.com>
2024-09-03 14:58:16 -0300

    Image and CSS folders creation, and navbar development

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,14 @@
+<link rel="stylesheet" href="{{asset('css/navbar.css')}}">
+
 <nav>
-    <div>
-        <a href="/">
-            <x-application-logo />
+    <div class="topnav" id="myTopnav">
+        <x-application-logo /> Autenticado
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
         </a>
     </div>
 </nav>
