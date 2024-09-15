ffe8b737196cbc4b598d0bc53a0dfa89f687c09f
Martin <martintamo627@gmail.com>
2024-09-15 10:56:49 -0300

    login small screen view update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,58 +6,65 @@
         <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     @endpush
 
-    <div class="content">
-        <div class= "childrens">
-            <img src="{{ asset('images/login/niños_pila.png') }}" alt="Niños en columna">
-        </div>
-        <div class="loginbox">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+    <div class="container">
+        <section class="side">
+            <div class= "childrens">
+                <img src="{{ asset('images/login/niños_pila.png') }}" alt="Niños en columna">
+            </div>
+        </section>
 
-                <div class="login-header">
-                    <h1>¡Bienvenido de nuevo!</h1>
-                </div>
+        <section class="main">
+            <div class="loginbox">
+                <form method="POST" action="{{ route('login') }}" class="login-form">
+                    @csrf
 
-                <div class= "user-login">
-                    <label for="email"></label>
-                    
-                    <input type="email" name="email" id="email" placeholder="Correo Electrónico" value="{{ old('email') }}" required autofocus
-                        autocomplete="username">
+                    <div class="login-header">
+                        <h1>¡Bienvenido de nuevo!</h1>
+                    </div>
 
-                    <x-input-error :messages="$errors->get('email')" />
-                </div>
+                    <div class= "user-login">
+                        <label for="email"></label>
+                        
+                        <input type="email" name="email" id="email" placeholder="Correo Electrónico" value="{{ old('email') }}" required autofocus
+                            autocomplete="username">
 
-                <div class="user-login">
-                    <label for="email"></label>
+                        <x-input-error :messages="$errors->get('email')" />
+                    </div>
 
-                    <input type="password" name="contraseña" id="contraseña" placeholder="Contraseña" required autocomplete="current-password">
+                    <div class="user-login">
+                        <label for="email"></label>
 
-                    <x-input-error :messages="$errors->get('contraseña')" />
-                </div>
+                        <input type="password" name="contraseña" id="contraseña" placeholder="Contraseña" required autocomplete="current-password">
 
-                <div class="remember">
-                    <label for="remember-me">
-                        <input id="remember-me" type="checkbox" name="remember" />
-                        <span>Recordarme la proxima vez</span>
-                    </label>
-                </div>
+                        <x-input-error :messages="$errors->get('contraseña')" />
+                    </div>
 
-                <div class="login-btn">
-                    <button type="submit">Iniciar sesion</button>
-                </div>
-    
-                <div class="forgot">            
-                    @if (Route::has('password.request'))
-                        <a id="forgot_password"
-                            href="{{ route('password.request') }}">
-                            ¿Olvidaste tu contraseña?
-                        </a>
-                    @endif
-                </div>
-                <div class="signup">
-                    ¿Todavia no tienes cuenta? <a href="{{ route('registrarse') }}">Regístrate</a>
-                </div>
-            </form>
-        </div>
+                    <div class="remember">
+                        <label for="remember-me">
+                            <input id="remember-me" type="checkbox" name="remember" />
+                            <span>Recordarme la proxima vez</span>
+                        </label>
+                    </div>
+
+                    <div class="login-btn">
+                        <button type="submit">Iniciar sesion</button>
+                    </div>
+        
+                    <div class="forgot">            
+                        @if (Route::has('password.request'))
+                            <a id="forgot_password"
+                                href="{{ route('password.request') }}">
+                                ¿Olvidaste tu contraseña?
+                            </a>
+                        @endif
+                    </div>
+                    <div class="signup">
+                        <p>¿Todavia no tienes cuenta? <a href="{{ route('registrarse') }}">Regístrate</a></p>
+                    </div>
+                </form>
+            </div>
+        </section>
+
+
     </div>
 </x-guest-layout>
