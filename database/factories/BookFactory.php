0ba01e349d47d13860d5a5b9e0480fb715852e7b
Martin <martintamo627@gmail.com>
2024-09-14 21:33:40 -0300

    book minor update

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -16,17 +16,17 @@ class BookFactory extends Factory
     public function definition(): array
     {
         return [
-            'ubicacion_fisica' => fake()->address(),
-            'titulo' => fake()->streetName(),
+            'ubicacion_fisica' => fake()->city(),
+            'titulo' => fake()->title(),
             'isbn' => fake()->isbn13(),
             'editorial' => fake()->firstNameFemale(),
             'año_edicion' => fake()->year(),
             'num_edicion' => fake()->numberBetween(1, 10),
-            'num_paginas' => fake()->paragraph(1),
+            'num_paginas' => fake()->numberBetween(1, 300),
             'lugar_edicion' => fake()->city(),
             'desc_primario' => fake()->lastName(),
             'idioma' => fake()->languageCode(),
-            'notas' => fake()->sentence(),
+            'notas' => fake()->text(),
             'id_disponibilidad' => BookAvailability::where('estado', BookAvailability::STATUS_AVAILABLE)->first()->id,
             'fecha_creacion' => fake()->date(),
             'fecha_edicion' => fake()->date()
