b4dfd3c2764ac4b3ceec19282c03ca3a695c465b
andrade <gabrieljoseandraderoso5@gmail.com>
2024-09-25 14:49:21 -0300

    add loan info downloader

--- resources/views/loan/show.blade.php
+++ resources/views/loan/show.blade.php
@@ -1,8 +1,5 @@
 <x-app-layout>
     <h1>Solicitaste {{$loan->book->titulo}}</h1>
     <p>Fecha: {{$loan->fecha_solicitud}}</p>
-    <form action="{{route('prestamo.generado.pdf', $loan->id)}}" method="post">
-        @csrf
-        <button>Descargar comprobante</button>
-    </form>
+    <x-download-loan-info loan_id="{{$loan->id}}" />
 </x-app-layout>
--- resources/views/profile/loans.blade.php
+++ resources/views/profile/loans.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     @push('styles')
-        <link rel="stylesheet" href="{{ asset('css/loan_view.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/loan_view.css') }}">
     @endpush
 
     <h1 class="loan-title">Mis préstamos</h1>
@@ -9,15 +9,11 @@
         <div class="loan-item">
             <a href="{{route('libro', $loan->book->id)}}">
                 <h2>{{$loan->book->titulo}}</h2>
-                </a>
-                <p>Solicitado el {{$loan->fecha_solicitud}}</p>
-                <p>Estado de solicitud: {{$loan->loanStatus->estado}}</p>
-                <form action="{{route('prestamo.generado.pdf', $loan->id)}}" method="post">
-                    @csrf
-                    <button>Descargar comprobante</button>
-                </form>
+            </a>
+            <p>Solicitado el {{$loan->fecha_solicitud}}</p>
+            <p>Estado de solicitud: {{$loan->loanStatus->estado}}</p>
+            <x-download-loan-info loan_id="{{$loan->id}}" />
         </div>
         @endforeach
-
     </div>
 </x-app-layout>
